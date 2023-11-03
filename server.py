87a0ce063eb62b1dbed22a2d707bdf4b6e23202f
Timothy Jacques <tjacques888@gmail.com>
2023-11-03 12:35:05 -0700

    Add current time to beginning of data

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from google.transit import gtfs_realtime_pb2
 import datetime, threading, requests, jsonify
-
-
+import time
 from secrets import API_KEY, API_PATH
 
 headers = {
@@ -18,18 +17,17 @@ lock = threading.Lock()
 
 @app.route('/', methods=['GET'])
 def get_bus():
-    print("hi")
-    print(busdata)
     lock.acquire()
     data = busdata
     lock.release()
+    data.insert(0, int(time.time()))
     return data
 
 def getData():
     r = requests.get(API_PATH,headers=headers)
     feed = gtfs_realtime_pb2.FeedMessage()
     feed.ParseFromString(r.content)
-    
+
     global busdata
     busdata = []
 
@@ -38,12 +36,12 @@ def getData():
         if entity.HasField('trip_update'):
             if entity.trip_update.trip.route_id == "2-13168":
                 for stu in entity.trip_update.stop_time_update:
-                    if stu.stop_sequence == 4 and stu.stop_id == "2442":      
+                    if stu.stop_sequence == 4 and stu.stop_id == "2442":
                         busdata.append(stu.arrival.time)
     busdata.sort()
-    threading.Timer(15, getData).start()
+    threading.Timer(20, getData).start()
     lock.release()
-    print(busdata[0])
+    print("received:", busdata); 
 
 with app.app_context():
    getData()

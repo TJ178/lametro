82443c473ba3343b239da5170106cfb9f4af79d9
Timothy Jacques <tjacques888@gmail.com>
2023-11-06 14:45:23 -0800

    Update display format to be more readable

--- client/client.ino
+++ client/client.ino
@@ -40,10 +40,16 @@ unsigned long timerDelay = 20000;
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+time_t* parseTimes(String s, int* numTimes);
+void displayTimes(time_t* busTimes, int numTimes);
+
 void setup() {
   Serial.begin(115200); 
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
+
+  int num;
+  parseTimes("[1699158516,1699160411,1699160412]", &num);
   
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -93,42 +99,20 @@ void loop() {
       
       // Send HTTP GET request
       int httpResponseCode = http.GET();
-
-      display.clearDisplay();
-    
-      display.setTextSize(3);
-      display.setTextColor(SSD1306_WHITE); // Draw white text
-      display.setCursor(0, 0);     // Start at top-left corner
-      display.cp437(true);         // Use full 256 char 'Code Page 437' font
       
       if (httpResponseCode>0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
         String payload = http.getString();
         Serial.println(payload);
-        //display.println(payload);
-        if(payload.length() > 2){
-          time_t busTime = (time_t) payload.substring(1, 11).toInt();  
-          
-          busTime -= 7*60*60;
 
-          Serial.println(busTime);
-          Serial.print(hourFormat12(busTime));
-          Serial.print(':');
-          Serial.println(minute(busTime));
-          
-          display.print(hourFormat12(busTime));
-          display.print(':');
-          display.println(minute(busTime));
-  
-          if(payload.length()>13){
-            time_t bus2Time = (time_t) payload.substring(12, 23).toInt();
-            bus2Time -= 7*60*60;
-            display.print(hourFormat12(bus2Time));
-            display.print(':');
-            display.println(minute(bus2Time));
-          }
-        }
+        int numTimes = 0;
+        time_t* busTimes = parseTimes(payload, &numTimes);
+        Serial.println("parsed");
+        Serial.println(numTimes);
+        displayTimes(busTimes, numTimes);
+        Serial.println("display");
+        free(busTimes);
       }
       else {
         Serial.print("Error code: ");
@@ -145,4 +129,68 @@ void loop() {
     }
     lastTime = millis();
   }
+}
+
+
+time_t* parseTimes(String s, int* numTimes){
+  if(s.length() <= 2){
+    *numTimes = 0;
+    return (time_t*) 0;
+  }
+  *numTimes = 1;
+  int idx = 0;
+  while(1){
+    idx = s.indexOf(',', idx) + 1;
+    if(idx == 0) break;
+    *numTimes = *numTimes + 1;
+  }
+
+  Serial.println(*numTimes);
+  time_t* out = (time_t*) malloc(sizeof(time_t) * *numTimes);
+
+  int lastIdx = 1;
+  for(int i = 0; i < *numTimes; i++){
+    idx = s.indexOf(',', lastIdx);
+    Serial.println(lastIdx);
+    Serial.println(idx);
+    if(idx == -1) idx = s.length()-1;
+    Serial.println(s.substring(lastIdx, idx));
+    out[i] = (time_t) s.substring(lastIdx, idx).toInt();
+    out[i] -= 7*60*60;
+    lastIdx = idx+1;
+  }
+  return out;
+}
+
+void displayTimes(time_t* busTimes, int numTimes){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  
+  if(numTimes <= 1){
+    display.print("No buses \ncurrently scheduled");
+  }else{
+    display.setTextSize(1);
+    display.println("Next bus:");
+    time_t nextMin = busTimes[1] - busTimes[0];
+    Serial.println(nextMin);
+    display.setTextSize(3);
+    display.print(minute(nextMin));
+    display.println(" min");
+
+    if(numTimes >= 3){
+      nextMin = busTimes[2] - busTimes[0];
+      if(minute(nextMin) > 15){
+        display.setTextSize(1);
+        char buff[20];
+        sprintf(buff, "Also at %d:%0d.2", hour(busTimes[2]), minute(busTimes[2]));
+        display.println(buff);
+      }else{
+        display.print(minute(nextMin));
+        display.println(" min");
+      }
+    }
+  }     
 }
